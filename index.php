d96116956bbbe919fa4c7b09cd7729a73ed6ade7
Nazar Korchevskiy <naaa.nazar@gmail.com>
2017-05-18 09:49:35 +0300

    add maxmind database api

--- index.php
+++ index.php
@@ -11,7 +11,26 @@
  * use
  *geoip service
  **/
+
 require ('src\freegeoip\FreeGeoIp.php');
 
 $freeGeoIp = new FreeGeoIp();
 var_dump($freeGeoIp->getIp());
+
+// maxmind
+require_once 'vendor/autoload.php';
+use GeoIp2\Database\Reader;
+
+
+// This creates the Reader object, which should be reused across
+// lookups.
+$reader = new Reader('GeoIP2-ISP.mmdb');
+
+$record = $reader->isp('8.8.8.8');
+
+print($record->autonomousSystemNumber . "\n");
+print($record->autonomousSystemOrganization . "\n"); // 'University of Minnesota'
+print($record->isp . "\n"); // 'University of Minnesota'
+//print($record->organization . "\n"); // 'University of Minnesota'
+
+print($record->ipAddress . "\n"); // '128.101.101.101'
